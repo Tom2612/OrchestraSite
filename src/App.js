0bb06300a79cc7d520c12837899ca8e9e04637ed
Tom <tomcat-26@hotmail.co.uk>
2022-11-01 20:43:46 +0800

    imported style.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import './style.css';
 
 function App() {
   return (
-    <div >
+    <div>
       <Navbar />
       <h1>Orchestra</h1>
       <Main />
