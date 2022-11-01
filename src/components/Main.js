34dc733fcb731bcd2ac0b86aa605f21a8389e393
Tom <tomcat-26@hotmail.co.uk>
2022-11-01 20:49:20 +0800

    added initial search components

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,14 @@ import React from 'react';
 export default function Main() {
     return (
         <main>
-            <h2>Main</h2>
+            <h2>Find your next piece!</h2>
+            <label for="composer">Search for composer: </label>
+            <select name="composer" id="composer">
+                <option value="">--Select Composer--</option>
+                <option value="beethoven">Beethoven</option>
+                <option value="mahler">Mahler</option>
+                <option value="sibelius">Sibelius</option>
+            </select>
         </main>
     )
 }
