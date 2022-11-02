f14d2ccf171e064d3d8ad926af7c4c24ad3232ea
Tom <tomcat-26@hotmail.co.uk>
2022-11-02 19:00:59 +0800

    Added my account link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,13 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <h2 className="navbar--title">Orchestra</h2>
-            <ul className="navbar--list">
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>Find pieces</a></li>
-            </ul>
+            <div className="navbar--menu">
+                <ul className="navbar--list">
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Find pieces</a></li>
+                </ul>
+                <a href="#" className="navbar--user">My account</a>
+            </div>
         </nav>
     )
 }
