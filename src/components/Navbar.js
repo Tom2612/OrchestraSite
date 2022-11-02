184d9898c5a33ef35615681fb1b9025200fab213
Tom <tomcat-26@hotmail.co.uk>
2022-11-02 18:55:37 +0800

    Updated Navbar.js and added Navbar.css with basic styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Navbar.css';
 
 export default function Navbar() {
     return (
-        <nav>
-            <h2>Navbar</h2>
+        <nav className="navbar">
+            <h2 className="navbar--title">Orchestra</h2>
+            <ul className="navbar--list">
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Find pieces</a></li>
+            </ul>
         </nav>
     )
 }
